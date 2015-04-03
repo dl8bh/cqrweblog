b6df7e887b92bab7b74f23892df64163c58fdd8a
Bernhard Hammes <mail@dl8bh.de>
2015-04-03 21:29:17 +0200

    validate qsocount better

--- inc/header.php
+++ inc/header.php
@@ -19,7 +19,11 @@ else
 	$log_id=$defaultlog;
 }
 if (isset($_GET['qso_count'])){
-$qso_count = htmlentities($_GET['qso_count']);
+$qso_count = filter_input(INPUT_GET, 'qso_count', FILTER_VALIDATE_INT);
+if (!is_int($qso_count))
+{
+		 $qso_count=$defaultcount;
+}
 }
 else
 {
