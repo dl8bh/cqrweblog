7a70e397d94c1e8c646e8874a9745b263e08de23
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2015-07-30 18:14:25 +0200

    made cluster colors configurable in settings, cleaned up example settings

--- inc/cluster.php
+++ inc/cluster.php
@@ -11,32 +11,44 @@ $out  = "";
 echo '<table class="cluster" align="center" >' . "\n";
 
 foreach($spots as $key => $qso){
-
+		$dxmessage='';
 		$clusterbandmode = freq_to_band_mode($qso[1]/1000);
-		$checkadif = check_adif($qso[10], $log_id, $clusterbandmode[0]);
+		$checkadif = check_adif($qso[10], $log_id, $clusterbandmode[0], $clusterbandmode[1]);
     switch ($checkadif[0]) {
 				case "N":
+				  // ATNO
 					$checkadif = check_adif($qso[10], $log_id);
 					if ($checkadif[0]=="N") {
-							$fontcolor='red';
+							$fontcolor=$atnocolor;
+							$dxmessage='<b><font color="' . $fontcolor . '">NEW ONE</font></b>';
+							break;
 					}
 					else {
-							$fontcolor='green';
+							$checkadif = check_adif($qso[10], $log_id, $clusterbandmode[0]);
+							if ($checkadif[0]!="N") {
+									$fontcolor=$newmodecolor;
+									$dxmessage='<b><font color="' . $fontcolor . '">NEW MODE</font></b>';
+									break;
+							}
+							else {
+									$fontcolor=$newbandcolor;
+									$dxmessage='<b><font color="' . $fontcolor . '">NEW BAND</font></b>';
+							}
 					}
 					break;
 				case "C":
-					$fontcolor='black';
+					$fontcolor=$confirmedcolor;
 					break;
 				case "W":
-					$fontcolor='blue';
+					$fontcolor=$workedcolor;
 					break;
 				default:
 		}
 		$out .= "<tr>";
-		
+		$out .= '<td>' . $dxmessage . '</td>	' ."\n";
 		$out .= '<td>DX de ' . $qso[0] . ':</td>' . "\n";
 		$out .= '<td>' . $qso[1] . '</td>' . "\n";
-		$out .=	'<td><a href=javascript:fillClusterData("' . $qso[2] . '","' . $qso[1] . '","' . $clusterbandmode[1] . '"); style="color:' . $fontcolor . '">' . $qso[2] . '</a></td>' . "\n";
+		$out .=	'<td><a href=javascript:fillClusterData("' . $qso[2] . '","' . $qso[1] . '","' . $clusterbandmode[1] . '"); style="color:' . $fontcolor . '; font-weight: bold;">' . $qso[2] . '</a></td>' . "\n";
 		$out .= '<td>' . $qso[3] . '</td>' . "\n";
 		$out .= '<td>' . $qso[4] . '</td>' . "\n";
 		foreach($qso as $subkey => $detail){
