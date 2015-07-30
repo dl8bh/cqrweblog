f2a3e58d96fd7fe2ece06bb5a4bbef91aa11cd88
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2015-07-30 17:19:19 +0200

    using new freq_to_band_mode in cluster

--- inc/cluster.php
+++ inc/cluster.php
@@ -1,6 +1,5 @@
 <div id="cluster">
 <?php
-//include("inc/include.php");
 
 if (isset($band)) {
 		$spots=get_cluster_spots($cluster_spot_num, $band);
@@ -13,9 +12,8 @@ echo '<table class="cluster" align="center" >' . "\n";
 
 foreach($spots as $key => $qso){
 
-		//$checkadif = check_adif($qso[10], $log_id, freq_to_band($qso[1]/1000), "ALL", "1", "1", "1");
-		$checkadif = check_adif($qso[10], $log_id, freq_to_band($qso[1]/1000));
-		$clustermode = freq_to_mode($qso[1]/1000);
+		$clusterbandmode = freq_to_band_mode($qso[1]/1000);
+		$checkadif = check_adif($qso[10], $log_id, $clusterbandmode[0]);
     switch ($checkadif[0]) {
 				case "N":
 					$checkadif = check_adif($qso[10], $log_id);
@@ -33,13 +31,12 @@ foreach($spots as $key => $qso){
 					$fontcolor='blue';
 					break;
 				default:
-//					$fontcolor='green';
 		}
 		$out .= "<tr>";
 		
 		$out .= '<td>DX de ' . $qso[0] . ':</td>' . "\n";
 		$out .= '<td>' . $qso[1] . '</td>' . "\n";
-		$out .=	'<td><a href=javascript:fillClusterData("' . $qso[2] . '","' . $qso[1] . '","' . $clustermode . '"); style="color:' . $fontcolor . '">' . $qso[2] . '</a></td>' . "\n";
+		$out .=	'<td><a href=javascript:fillClusterData("' . $qso[2] . '","' . $qso[1] . '","' . $clusterbandmode[1] . '"); style="color:' . $fontcolor . '">' . $qso[2] . '</a></td>' . "\n";
 		$out .= '<td>' . $qso[3] . '</td>' . "\n";
 		$out .= '<td>' . $qso[4] . '</td>' . "\n";
 		foreach($qso as $subkey => $detail){
