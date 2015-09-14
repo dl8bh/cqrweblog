70228a0e2d9074b9b89f74ec2d63283c3943d206
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2015-09-14 17:39:15 +0200

    add shortcut: space in frequency field puts focus on callsign

--- inc/addshortcuts.js
+++ inc/addshortcuts.js
@@ -46,6 +46,14 @@ document.input.name.focus();
 'target':document.input.mode
 });
 
+shortcut.add("SPACE",function() {
+document.input.call.focus();
+},{
+'type':'keydown',
+'propagate':false,
+'target':document.input.frequency,
+});
+
 shortcut.add("TAB",function() {
 document.input.frequency.focus();
 },{
