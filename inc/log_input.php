42fd2e8f8e1a3c2c7917e6195ae7ff58fdc225a3
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2015-10-29 22:10:19 +0100

    improve input form design

--- inc/log_input.php
+++ inc/log_input.php
@@ -7,9 +7,10 @@ if (isset($qso_count)) {
 		$urlparameter .= '&qso_count=' . $qso_count;
 }
 
-echo '<form class="form-inline" role="form" name="input" action="log.php' . $urlparameter . '" method="post">';
-?>
-	<div class="form-group " >
+echo '<form class="form" role="form" name="input" action="log.php' . $urlparameter . '" method="post">';
+?> 
+	<div class="row no-gutter">
+	<div class="form-group col-sm-1 " >
 	<label for="band" class="control-label">Band</label>
 	<select class="form-control" id="band" name="band" tabindex=1>
 	<?php
@@ -24,11 +25,11 @@ echo '<form class="form-inline" role="form" name="input" action="log.php' . $url
 	?>
 	</select>
 	</div>
-	<div class="form-group ">
-    <label for="time" class="control-label">Time</label>
-	<input id="time" class="form-control" onClick="this.setSelectionRange(0,this.value.length)" type="text" name="time" size="5" value="" maxlength="5" tabindex="1" title="optional, if empty: time in UTC, input format: hh:mm or hhmm">
+	<div class="form-group col-sm-1 ">
+    <label for="time" class="control-label" >Time</label>
+	<input id="time" class="form-control" placeholder="HHMM" onClick="this.setSelectionRange(0,this.value.length)" type="text" name="time" size="5" value="" maxlength="5" tabindex="1" title="optional, if empty: time in UTC, input format: hh:mm or hhmm">
 </div>
-  <div class="form-group <?php if (!isset($freq)) { echo "has-error has-feedback" ; }  ?>">	
+  <div class="form-group col-sm-1 <?php if (!isset($freq)) { echo "has-error has-feedback" ; }  ?>">	
     <label for="freq" class="control-label">Frequency</label>
 	<?php
 	if (isset($freq))
@@ -42,12 +43,12 @@ echo '<form class="form-inline" role="form" name="input" action="log.php' . $url
 	}
 	?>
 	</div>
-  <div class="form-group <?php if (empty($call)) { echo "has-error has-feedback" ; }  ?>">
+  <div class="form-group col-sm-2 <?php if (empty($call)) { echo "has-error has-feedback" ; }  ?>">
     <label for="call" class="control-label">Callsign</label>
 	<input id="call" class="form-control	" type="text" maxlength="55" size="15" name="call" tabindex="2" onchange="data_copy()" autofocus >
 	<?php if (empty($call)) { echo' <span class="glyphicon glyphicon-remove form-control-feedback"></span>';} ?>
 	</div>
-  <div class="form-group ">
+  <div class="form-group col-sm-1 ">
     <label for="mode">Mode</label>
 	<?php
 	if (empty($mode))
@@ -66,16 +67,20 @@ echo '<form class="form-inline" role="form" name="input" action="log.php' . $url
 			$rapport=$default_cw_rapport;
 	}
 	echo '<input onClick="enable_space(); this.setSelectionRange(0,this.value.length); enable_space();" id="mode" class="form-control" type="text" name="mode" size="5" value="' . $mode . '" tabindex="3" maxlength="7"></div>';
-	echo '<div class="form-group "><label for="rst_sent">RST_S</label>';
+	echo '<div class="form-group col-sm-1"><label for="rst_sent">RST_S</label>';
 	echo '<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="rst_sent" name="rst_sent" size="8" value="' . $rapport  . '" maxlength="10" tabindex="4"></div>';
-	echo '<div class="form-group "><label for="rst_rcvd">RST_R</label>';
+	echo '<div class="form-group col-sm-1 "><label for="rst_rcvd">RST_R</label>';
 	echo '<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="rst_rcvd" name="rst_rcvd" size="8" value="' . $rapport . '" maxlength="10" tabindex="5"></div>';
 	?>
-	<div class="form-group "><label for="name">Name</label>
+	<div class="form-group col-xs-2 "><label for="name">Name</label>
 	<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="name" name="name" maxlength="55" tabindex="6"></div>
-	<div class="form-group "><label for="remarks">Remarks</label>
+	<div class="form-group col-sm-2 "><label for="remarks">Remarks</label>
 	<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="remarks" name="remarks" maxlength="55" tabindex="7"></div>
-	<input class="btn btn-primary " type="submit" value="Log">
+	</div>
+	<div class="row">
+	<div class="col-sm-11"></div>
+	<input class="btn btn-primary col-sm-1 " type="submit" value="Log">
+	</div>
 	</form>
 
 
@@ -85,11 +90,11 @@ echo '<form class="form-inline" role="form" name="input" action="log.php' . $url
 <?php
 	if ($altstats[$log_id])
 	{
-			echo '<form name="stats" action="stats2new.php' . $urlparameter . '" target="_blank"  method="post">';
+			echo '<form name="stats" action="stats2.php' . $urlparameter . '" target="_blank"  method="post">';
 	}
 	else
 	{
-			echo '<form name="stats" action="statsnew.php' . $urlparameter . '" target="_blank"  method="post">';
+			echo '<form name="stats" action="stats.php' . $urlparameter . '" target="_blank"  method="post">';
 	}
 	echo '<input class="btn btn-info" type="submit" value="Check DXCC" accesskey="C" title="shortcut browser accesskey + C">';
 
@@ -99,7 +104,7 @@ echo '</form></div>' . "\n" ;
 echo '' . "\n";
 
 
-echo '<div class="col-sm-1"><form name="search" action="logsearch2.php' . $urlparameter . '" target="_blank"  method="post">';
+echo '<div class="col-sm-1"><form name="search" action="logsearch.php' . $urlparameter . '" target="_blank"  method="post">';
 echo '<input class="btn btn-info" type="submit" value="In log?" accesskey="S" title="shortcut browser accesskey + S" >' . "\n";
 echo '<input type="hidden" name="call" value="">' . "\n";
 echo '<input type="hidden" name="inlog" value="1">' . "\n";
