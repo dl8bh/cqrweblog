2674b4820c0631b31d66a68d5064ff2754b16436
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2015-10-30 20:33:36 +0100

    code refurbish

--- inc/log_input.php
+++ inc/log_input.php
@@ -64,27 +64,62 @@ echo '<form class="form" role="form" name="input" action="index.php' . $urlparam
 	else {
 			$rapport=$default_cw_rapport;
 	}
-	echo '<input onClick="enable_space(); this.setSelectionRange(0,this.value.length); enable_space();" id="mode" class="form-control" type="text" name="mode" size="5" value="' . $mode . '" tabindex="3" maxlength="7"></div>';
-	echo '<div class="form-group col-sm-1"><label for="rst_sent">RST_S</label>';
-	echo '<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="rst_sent" name="rst_sent" size="8" value="' . $rapport  . '" maxlength="10" tabindex="4"></div>';
-	echo '<div class="form-group col-sm-1 "><label for="rst_rcvd">RST_R</label>';
-	echo '<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="rst_rcvd" name="rst_rcvd" size="8" value="' . $rapport . '" maxlength="10" tabindex="5"></div>';
 	?>
-	<div class="form-group col-sm-1 "><label for="name">Name</label>
-	<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="name" name="name" maxlength="55" tabindex="6"></div>
-	<div class="form-group col-sm-2 "><label for="remarks">Remarks</label>
-	<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="remarks" name="remarks" maxlength="55" tabindex="7"></div>
+		<input onClick="enable_space(); this.setSelectionRange(0,this.value.length); enable_space();" id="mode" class="form-control" type="text" name="mode" size="5" value="<?php echo $mode ;?>" tabindex="3" maxlength="7">
+	</div>
+
+	
+	<div class="form-group col-sm-1">
+			<label for="rst_sent" class="control-label">RST_S</label>
+			<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="rst_sent" name="rst_sent" size="8" value="<?php echo $rapport ;?>  " maxlength="10" tabindex="4">
+	</div>
+
+	
+	<div class="form-group col-sm-1 ">
+			<label for="rst_rcvd" class="control-label">RST_R</label>
+			<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="rst_rcvd" name="rst_rcvd" size="8" value="<?php echo $rapport; ?>" maxlength="10" tabindex="5">
+	</div>
+	
+	<div class="form-group col-sm-1 ">
+			<label for="name">Name</label>
+			<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="name" name="name" maxlength="55" tabindex="6">
+	</div>
+	
+	<div class="form-group col-sm-2 ">
+			<label for="remarks">Remarks</label>
+			<input onClick="this.setSelectionRange(0,this.value.length)" type="text" class="form-control" id="remarks" name="remarks" maxlength="55" data-toggle="tooltip" title="I:IOTA L:LOCATOR M:MANAGER S:STATE" tabindex="7"></div>
 	</div>
 	<div class="row">
-	<div class="col-sm-10"></div>
-	<input class="btn btn-primary col-sm-1 " type="submit" value="Log">
+			<div class="col-sm-10"></div>
+			<input class="btn btn-primary col-sm-1 " type="submit" value="Log">
 	</div>
 	</form>
 
 
 </div>
 </div>
-<div class="row"><div class="col-sm-3"></div><div class="col-sm-1">
+<div class="row">
+	<div class="col-sm-3">
+
+<?php
+if ($enable_help[$log_id]) {
+/*		echo '</br>' ."\n" ;
+		echo 'Shortcuts: Alt+w/F11: Wipe Alt+q: qrz.com </br>' . "\n";
+		echo 'Remarks-Strings: I:IOTA L:LOCATOR M:MANAGER S:STATE</br>' . "\n";
+		echo '<a href="./README.txt" target="_blank" >Complete Manual</a> ' . "\n"; */
+	?>
+	<button type="button" class="btn btn-info" data-toggle="collapse" data-target="#help">Show help</button>
+  <div id="help" class="collapse">
+	Wipe: <kbd>Alt+w</kbd> <kbd>F11</kbd></br>
+	QRZ: <kbd>Alt+q</kbd> </br>
+	Remarks-Strings: I:IOTA L:LOCATOR M:MANAGER S:STATE</br>
+	<a href="./README.txt" target="_blank" >Complete Manual</a>
+	</div>
+	<?php
+}
+?>
+		</div>
+		<div class="col-sm-1">
 <?php
 	if ($altstats[$log_id])
 	{
@@ -94,28 +129,29 @@ echo '<form class="form" role="form" name="input" action="index.php' . $urlparam
 	{
 			echo '<form name="stats" action="stats.php' . $urlparameter . '" target="_blank"  method="post">';
 	}
-	echo '<input class="btn btn-info" type="submit" value="Check DXCC" accesskey="C" data-toggle="tooltip" title="shortcut browser accesskey + C">';
+	?>
+	<input class="btn btn-info" type="submit" value="Check DXCC" accesskey="C" data-toggle="tooltip" title="shortcut browser accesskey + C">
+	<input type="hidden" name="callsign" value="">
+	<input type="hidden" name="inlog" value="1"> 
+		</form></div>
 
-echo '<input type="hidden" name="callsign" value="">' . "\n";
-echo '<input type="hidden" name="inlog" value="1">' . "\n";
-echo '</form></div>' . "\n" ;
-echo '' . "\n";
+				<div class="col-sm-1">
+					<form name="search" action="logsearch.php<?php echo $urlparameter ;?> " target="_blank"  method="post">
+						<input class="btn btn-info col-sm-12" type="submit" value="In log?" accesskey="S" data-toggle="tooltip" title="shortcut browser accesskey + S" >
+						<input type="hidden" name="call" value=""> 
+						<input type="hidden" name="inlog" value="1">
+					</form>
+				</div>
 
-
-echo '<div class="col-sm-1"><form name="search" action="logsearch.php' . $urlparameter . '" target="_blank"  method="post">';
-echo '<input class="btn btn-info col-sm-12" type="submit" value="In log?" accesskey="S" data-toggle="tooltip" title="shortcut browser accesskey + S" >' . "\n";
-echo '<input type="hidden" name="call" value="">' . "\n";
-echo '<input type="hidden" name="inlog" value="1">' . "\n";
-echo '</form></div>';
-?>
-<input class="btn btn-info col-sm-1" data-toggle="tooltip" type="submit" value="QRZ?" onclick="qrz_call()" title="shortcut: Alt+q">
-<div class="col-sm-4"></div>
-<input class="btn btn-warning col-sm-1" data-toggle="tooltip" type="submit" value="Cancel" onclick="wipe_data()" title="shortcut: Alt+w/F11">
+				<input class="btn btn-info col-sm-1" data-toggle="tooltip" type="submit" value="QRZ?" onclick="qrz_call()" title="shortcut: Alt+q">
+				<div class="col-sm-4"></div>
+				<input class="btn btn-warning col-sm-1" data-toggle="tooltip" type="submit" value="Cancel" onclick="wipe_data()" title="shortcut: Alt+w/F11">
 </div>
 <?php
 if ($enable_help[$log_id]) {
-		echo '</br>' ."\n" ;
+/*		echo '</br>' ."\n" ;
 		echo 'Shortcuts: Alt+w/F11: Wipe Alt+q: qrz.com </br>' . "\n";
 		echo 'Remarks-Strings: I:IOTA L:LOCATOR M:MANAGER S:STATE</br>' . "\n";
-		echo '<a href="./README.txt" target="_blank" >Complete Manual</a> ' . "\n";
+		echo '<a href="./README.txt" target="_blank" >Complete Manual</a> ' . "\n"; */
 }
+?>
