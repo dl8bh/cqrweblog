a522124cba3703261a780fcefc4172a25108f25b
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2015-10-30 20:45:08 +0100

    fix small css bug

--- inc/metaheader.php
+++ inc/metaheader.php
@@ -1,9 +1,10 @@
 
 <meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="inc/css/log.css">
 <script src="inc/shortcut.js" type="text/javascript" name="shortcut/shortcut"></script>
 <script src="inc/copy.js" type="text/javascript" name="copycall/copycall"></script>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="inc/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="inc/log2.css">
+<link rel="stylesheet" type="text/css" href="inc/css/bootstrapmodify.css">
 
 
