1295ddef7931bbef2dc441838daf151662fa16c1
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2015-11-01 19:29:56 +0100

    fix path

--- inc/metafooter.php
+++ inc/metafooter.php
@@ -1,5 +1,5 @@
-<script src="inc/shortcut.js" type="text/javascript" name="shortcut/shortcut"></script>
-<script src="inc/copy.js" type="text/javascript" name="copycall/copycall"></script>
+<script src="inc/js/shortcut.js" type="text/javascript" name="shortcut/shortcut"></script>
+<script src="inc/js/copy.js" type="text/javascript" name="copycall/copycall"></script>
 <script src="inc/js/jquery.min.js"></script>
 <script src="inc/js/bootstrap.min.js"></script>
 <script>
