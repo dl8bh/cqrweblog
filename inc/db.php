0ce8df4a28bd910335f759f7b0c2f083797a3d52
Bernhard Kneip <mail@dl8bh.de>
2021-05-16 20:17:30 +0200

    every class gets its own mysqli object

--- inc/db.php
+++ inc/db.php
@@ -1,10 +1,31 @@
 <?php
 $dbconnect = mysqli_connect($hostname, $dbuser, $dbpass, $db, $port);
-$dbobj = new mysqli($hostname, $dbuser, $dbpass, $db, $port);
 if(!$dbconnect)
 {
   exit("verbindungsfehler: ".mysqli_connect_error());
 }
 
+class DbSpawner {
+    /* spawns mysqli-objects for classes */
+    private $hostname;
+    private $dbuser;
+    private $dbpass;
+    private $dbname;
+    private $dbport;
+
+    function __construct($hostname, $dbuser, $dbpass, $dbname, $dbport){
+        $this->hostname = $hostname;
+        $this->dbuser = $dbuser;
+        $this->dbpass = $dbpass;
+        $this->dbport = $dbport;
+        $this->dbname = $dbname;
+    }
+
+    function newdb() {
+        return new mysqli($this->hostname, $this->dbuser, $this->dbpass, $this->dbname, $this->dbport);
+    }
+}
+
+$DbSpawner = new DbSpawner($hostname, $dbuser, $dbpass, $db, $port);
 
 ?>
--- inc/include.php
+++ inc/include.php
@@ -85,20 +85,8 @@ function call_to_dxcc ( $callsign) {
 }
 
 function get_iota ( $call, $pref ) {
-		
-		global $dbconnect;
-		
-		$dbconnect -> select_db("cqrlog_common");
-		$call = mysqli_real_escape_string($dbconnect ,$call);
-		$ergebnis = mysqli_query($dbconnect, 'SELECT * FROM iota_list WHERE dxcc_ref="' . $pref . '" AND "' . $call . '" RLIKE CONCAT("^",pref) AND pref !=""'  );	
-		
-		while($row = mysqli_fetch_object($ergebnis)) {
-			$iota_nr = $row->iota_nr;
-			$iota_name = $row->island_name;
-			return array ( $iota_nr, $iota_name );
-		}
-		
-		return array ( NULL , NULL);
+    global $Cqrlog_common;
+    return $Cqrlog_common->get_iota($call, $pref);
 }
 
 function count_qsos ( $log_id ) {
@@ -120,156 +108,50 @@ function count_qsos ( $log_id ) {
 }
 
 function logid_to_call ( $log_id ) {
-		
-		global $dbconnect;		
-		$dbconnect -> select_db("cqrlog_common" );
-		
-		$log_id = mysqli_real_escape_string($dbconnect ,$log_id);
-		$ergebnis = mysqli_query($dbconnect, "SELECT log_name FROM log_list WHERE log_nr=" . $log_id);
-		while($row = mysqli_fetch_object($ergebnis)) {
-				return $row->log_name;
-		}
-				
-				return NULL;
+    global $Cqrlog_common;
+    return $Cqrlog_common->logid_to_call($log_id);
 }
 
 function freq_to_band_mode ( $inputfreq ) {
-				
-		global $dbconnect;
-		$dbconnect -> select_db("cqrlog_common");
-		
-		$inputfreq = mysqli_real_escape_string($dbconnect ,$inputfreq);
-		$ergebnis = mysqli_query($dbconnect, "select cw,rtty,ssb,band from bands where b_begin <= " . $inputfreq . " and b_end >=" . $inputfreq  );
-		
-		while($row = mysqli_fetch_object($ergebnis)) {
-				$returnband=$row->band;
-				if ($inputfreq < $row->cw) {
-						$returnmode = "CW";
-				}
-				
-				else if (($inputfreq >= $row->rtty)&&($inputfreq < $row->ssb)) {
-						$returnmode = "RTTY";
-				}
-				
-				else if ($inputfreq > $row->ssb) {
-						$returnmode = "SSB";
-				}
-
-				return array ($returnband, $returnmode);
-		}
-
-		return NULL ;
+    global $Cqrlog_common;
+    return $Cqrlog_common->freq_to_band_mode($inputfreq);
 }
 
 
 function freq_to_band ( $inputfreq ) {
-		$bandmode = freq_to_band_mode($inputfreq);
-		return $bandmode[0];
+    global $Cqrlog_common;
+    return $Cqrlog_common->freq_to_band($inputfreq);
 }
 
 
 function freq_to_mode ( $inputfreq ) {
-		
-		$bandmode = freq_to_band_mode($inputfreq);
-		return $bandmode[1];
+    global $Cqrlog_common;
+    return $Cqrlog_common->freq_to_mode($inputfreq);
 }
 
 function get_manager ( $call ) {
-
-		global $dbconnect;		
-		$dbconnect -> select_db("cqrlog_common" );
-		
-		$call = mysqli_real_escape_string($dbconnect ,$call);
-		$ergebnis = mysqli_query($dbconnect, 'SELECT qsl_via FROM qslmgr WHERE callsign ="' . $call . '"');
-		
-		while($row = mysqli_fetch_object($ergebnis)) {
-				return $row->qsl_via;
-		}
-		
-		return NULL;
+    global $Cqrlog_common;
+    return $Cqrlog_common->get_manager($call);
 }
 
 function band_to_freq ( $inputband ) {
-		
-		global $dbconnect;
-		$dbconnect -> select_db("cqrlog_common");
-		
-		$inputband = mysqli_real_escape_string($dbconnect ,$inputband);
-		$ergebnis = mysqli_query($dbconnect, "select b_begin from bands where band = '" . $inputband ."'"  );
-		
-		while($row = mysqli_fetch_object($ergebnis)) {
-				return $row->b_begin;
-		}
-
-		return NULL;
+    global $Cqrlog_common;
+    return $Cqrlog_common->band_to_freq($inputband);
 }
 
 function validate_freq ( $freq ) {
-		
-		switch ($freq) {
-			//check for lazy WARC band frequency input
-			case '10' :
-				return band_to_freq( '30M' );
-				break;
-			
-			case '18' :
-				return band_to_freq( '17M' );
-				break;
-			
-			case '24' :
-				return band_to_freq( '12M' );
-			
-			case '' :
-				return NULL;
-			
-			//Check if frequency input was in kHz, if true, convert to MHz
-			default:
-			if (!freq_to_band($freq)) {
-					
-					//kHz/1000 = MHz
-					$newfreq=$freq/1000;
-					
-					if (freq_to_band( $newfreq )) {
-							
-							return $newfreq;
-					}
-			}
-			
-			return $freq;
-			break;
-		}
+    global $Cqrlog_common;
+    return $Cqrlog_common->validate_freq($freq);
 }
 
 function dxcc_to_adif ( $dxcc ) {
-
-		global $dbconnect;
-		$dbconnect -> select_db("cqrlog_common");
-	  
-		$dxcc = mysqli_real_escape_string($dbconnect ,$dxcc);
-		$ergebnis = mysqli_query($dbconnect, "select adif from dxcc_ref where pref = '" . $pref ."'"  );
-		
-		while($row = mysqli_fetch_object($ergebnis)) {
-				
-				return $row->adif;
-		}
-
-		return NULL;
+    global $Cqrlog_common;
+    return $Cqrlog_common->dxcc_to_adif($dxcc);
 }
 
 function adif_to_dxcc ( $adif ) {
-		
-		global $dbconnect;
-		$dbconnect -> select_db("cqrlog_common");
-		
-		$adif = mysqli_real_escape_string($dbconnect , $adif);
-		$ergebnis = mysqli_query($dbconnect, "select pref from dxcc_ref where adif = '" . $adif ."'"  );
-		
-		while($row = mysqli_fetch_object($ergebnis)) {
-				
-				return $row->pref;
-		}
-
-		return NULL;
+    global $Cqrlog_common;
+    return $Cqrlog_common->dxcc_to_adif($adif);
 }
 
 function qslstring ( $paper, $lotw, $eqsl ) {
