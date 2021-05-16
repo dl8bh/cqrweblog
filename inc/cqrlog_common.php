de3b474b08e539f9d3384c2505bac2361ac8147d
Bernhard Kneip <mail@dl8bh.de>
2021-05-16 16:20:54 +0200

    cqrlog_common: refactor logid_to_call

--- inc/cqrlog_common.php
+++ inc/cqrlog_common.php
@@ -31,18 +31,15 @@ class Cqrlog_common {
         return array ($iota_nr, $island_name);
     }
     
-    function logid_to_call ( $log_id ) {
+    function logid_to_call (int $log_id) {
             
-            global $dbconnect;		
-            $dbconnect -> select_db("cqrlog_common" );
-            
-            $log_id = mysqli_real_escape_string($dbconnect ,$log_id);
-            $ergebnis = mysqli_query($dbconnect, "SELECT log_name FROM log_list WHERE log_nr=" . $log_id);
-            while($row = mysqli_fetch_object($ergebnis)) {
-                    return $row->log_name;
+            $log_id = (int) $this->dbobj->real_escape_string($log_id);
+            $query = sprintf("SELECT log_name FROM log_list WHERE log_nr=%u", $log_id);
+            $result = $this->dbobj->query($query);
+            if ($result->num_rows) {
+                return $result->fetch_object->log_name;
             }
-                    
-                    return NULL;
+            return NULL;
     }
     
     function freq_to_band_mode ( $inputfreq ) {
