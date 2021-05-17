8a3ecf7facc828d9530db582806c7b0b6b04f90a
Bernhard Kneip <mail@dl8bh.de>
2021-05-17 14:03:53 +0200

    begin logbook.php class

---
+++ inc/logbook.php
@@ -0,0 +1,33 @@
+<?php
+
+class Logbook
+{
+    private $dbobj;
+    private $log_id;
+
+    function __construct($dbobj, int $log_id)
+    {
+        $this->dbobj = $dbobj;
+        $this->log_id = $log_id;
+        $this->database_name = $this->logid_to_tableid($log_id);
+        $this->dbobj->select_db($this->database_name);
+    }
+
+    function logid_to_tableid(int $log_id)
+    {
+        $log_id = sprintf("%03d", $log_id);
+        return 'cqrlog' . $log_id;
+    }
+
+
+
+    function count_qsos()
+    {
+        $where = "1 = 1";
+        $query = sprintf("SELECT COUNT(*) FROM view_cqrlog_main_by_qsodate WHERE %s", $where);
+        $result = $this->dbobj->query($query);
+        $result = $result->fetch_row();
+
+        return $result[0];
+    }
+}
