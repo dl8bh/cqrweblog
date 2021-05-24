a946adf69ef1d169bfbd7e56a99956609da2c0ba
Bernhard Kneip <mail@dl8bh.de>
2021-05-24 22:15:31 +0200

    cluster: switchted to new oo implementations

--- inc/cluster.php
+++ inc/cluster.php
@@ -1,69 +1,62 @@
-<div id="cluster" class="table-responsive" >
-<?php
+<div id="cluster" class="table-responsive">
+    <?php
 
-if (isset($band)) {
-		$spots=get_cluster_spots($cluster_spot_num, $band);
-}
-else {
-		$spots=get_cluster_spots($cluster_spot_num, "ALL");
-}
-$out  = "";
-echo '<table class="table table-hover borderless table-condensed" align="center" >' . "\n";
-
-foreach($spots as $key => $qso){
-		$dxmessage='';
-		$clusterbandmode = $Cqrlog_common->freq_to_band_mode($qso[1]/1000);
-		$checkadif = check_adif($qso[10], $log_id, $clusterbandmode[0], $clusterbandmode[1]);
-    if (check_dupe($log_id, $qso[2], $clusterbandmode[0], $clusterbandmode[1])) {
-			$fontcolor=$dupecolor;		
-			$dxmessage='<b><font color="' . $fontcolor . '">DUPE</font></b>';
-			}
-		else {
-		switch ($checkadif[0]) {
-				case "N":
-					$checkadif = check_adif($qso[10], $log_id);
-					if ($checkadif[0]=="N") {
-							$fontcolor=$atnocolor;
-							$dxmessage='<b><font color="' . $fontcolor . '">NEW ONE</font></b>';
-							break;
-					}
-					else {
-							$checkadif = check_adif($qso[10], $log_id, $clusterbandmode[0]);
-							if ($checkadif[0]!="N") {
-									$fontcolor=$newmodecolor;
-									$dxmessage='<b><font color="' . $fontcolor . '">NEW MODE</font></b>';
-									break;
-							}
-							else {
-									$fontcolor=$newbandcolor;
-									$dxmessage='<b><font color="' . $fontcolor . '">NEW BAND</font></b>';
-							}
-					}
-					break;
-				case "C":
-					$fontcolor=$confirmedcolor;
-					break;
-				case "W":
-					$fontcolor=$workedcolor;
-					break;
-				default:
-		}
-		}
-		$out .= '<tr class="small">';
-		$out .= '<td class="hidden-xs">' . $dxmessage . '</td>	' ."\n";
-		$out .= '<td class="hidden-xs">DX de ' . $qso[0] . ':</td>' . "\n";
-		$out .= '<td>' . $qso[1] . '</td>' . "\n";
-		$out .=	'<td><a href="javascript:fillClusterData(\'' . $qso[2] . '\',\'' . $qso[1] . '\',\'' . $clusterbandmode[1] . '\');" style="color:' . $fontcolor . '; font-weight: bold;">' . $qso[2] . '</a></td>' . "\n";
-		$out .= '<td>' . $qso[3] . '</td>' . "\n";
-		$out .= '<td>' . $qso[4] . '</td>' . "\n";
-		foreach($qso as $subkey => $detail){
-				
+    if (isset($band)) {
+        $spots = get_cluster_spots($cluster_spot_num, $band);
+    } else {
+        $spots = get_cluster_spots($cluster_spot_num, "ALL");
     }
-    $out .= "</tr>"."\n";
-}
-$out .= "</table>";
+    $out  = "";
+    echo '<table class="table table-hover borderless table-condensed" align="center" >' . "\n";
 
-echo $out;
-?>
+    foreach ($spots as $key => $qso) {
+        $dxmessage = '';
+        $clusterbandmode = $Cqrlog_common->freq_to_band_mode($qso[1] / 1000);
+        $checkadif = $Logbook->get_stats($qso[10], array());
+        $qsoarray = array(
+            "callsign" => $qso[2],
+            "band" => $clusterbandmode[0],
+            "mode" => $clusterbandmode[1]
+        );
+        $band = $clusterbandmode[0];
+        $mode = $clusterbandmode[1];
+        $qsoobject = new Qso($qsoarray);
+        if ($Logbook->check_dupe($qsoobject)) {
+            $fontcolor = $dupecolor;
+            $dxmessage = '<b><font color="' . $fontcolor . '">DUPE</font></b>';
+        } elseif (!isset($checkadif[$qso[10]])) {
+            $fontcolor = $atnocolor;
+            $dxmessage = '<b><font color="' . $fontcolor . '">NEW ONE</font></b>';
+        } elseif (!isset($checkadif[$qso[10]][$band][$mode])) {
+            if (!isset($checkadif[$qso[10]][$band])) {
+                $fontcolor = $newbandcolor;
+                $dxmessage = '<b><font color="' . $fontcolor . '">NEW BAND</font></b>';
+            } elseif (!isset($checkadif[$qso[10]][$mode])) {
+                $fontcolor = $newmodecolor;
+                $dxmessage = '<b><font color="' . $fontcolor . '">NEW MODE</font></b>';
+            }
+        } else {
+            switch ($checkadif[$qso[10]][$band][$mode]) {
+                case "C":
+                    $fontcolor = $confirmedcolor;
+                case "W":
+                    $fontcolor = $workedcolor;
+            }
+        }
+
+ 
+        
+        $out .= '<tr class="small">';
+        $out .= '<td class="hidden-xs">' . $dxmessage . '</td>	' . "\n";
+        $out .= '<td class="hidden-xs">DX de ' . $qso[0] . ':</td>' . "\n";
+        $out .= '<td>' . $qso[1] . '</td>' . "\n";
+        $out .=    '<td><a href="javascript:fillClusterData(\'' . $qso[2] . '\',\'' . $qso[1] . '\',\'' . $clusterbandmode[1] . '\');" style="color:' . $fontcolor . '; font-weight: bold;">' . $qso[2] . '</a></td>' . "\n";
+        $out .= '<td>' . $qso[3] . '</td>' . "\n";
+        $out .= '<td>' . $qso[4] . '</td>' . "\n";
+        $out .= "</tr>" . "\n";
+    }
+    $out .= "</table>";
+
+    echo $out;
+    ?>
 </div>
-
