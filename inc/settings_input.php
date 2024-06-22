4e29cd648b1c5b7450a0d75b9cdf973cbf981402
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2024-06-22 11:53:43 +0200

    implementation of more input fields, fix typos

--- inc/settings_input.php
+++ inc/settings_input.php
@@ -1,5 +1,6 @@
 <?php
-echo '<form class="form" role="form" action="logsearch.php?log_id=' . $log_id . '&qso_count=' . $qso_count . '" method="post">'
+echo '<form class="form" role="form" action="settings.php?log_id=' . $log_id . '&qso_count=' . $qso_count . '" method="post">';
+//print_r($_POST);
 ?>
 <div class="row">
 
@@ -8,8 +9,8 @@ echo '<form class="form" role="form" action="logsearch.php?log_id=' . $log_id . 
 
 
         <div class="form-group col-md-1 col-sm-2 col-xs-3">
-            <label for="cluste_spot_count" class="control-label"># of spots</label>
-            <input onClick="this.setSelectionRange(0,this.value.length)" class="form-control" id="cluster_enable" value="<?php echo ($Userconfig->get_cluster_spot_number()) ?>" type="text" maxlength="55" size="15" name="call" data-toggle="tooltip" title="use n for number of spots, 0 for cluster disable">
+            <label for="cluster_spot_count" class="control-label"># of spots</label>
+            <input onClick="this.setSelectionRange(0,this.value.length)" class="form-control" id="cluster_spot_count" value="<?php echo ($Userconfig->get_cluster_spot_number()) ?>" type="text" maxlength="55" size="15" name="cluster_spot_count" data-toggle="tooltip" title="use n for number of spots, 0 for cluster disable">
         </div>
         <div class="form-group col-md-2 col-sm-2 col-xs-6">
             <label for="cluster_mode" class="control-label">Cluster Mode</label>
@@ -84,17 +85,17 @@ echo '<form class="form" role="form" action="logsearch.php?log_id=' . $log_id . 
         </div>
 
 
-        <div class="form-group col-sm-1 col-xs-4">
-            <label for="pubsearch" class="control-label">Pubsearch</label>
+        <div class="form-group col-sm-2 col-xs-4">
+            <label for="pubsearch" class="control-label">Public Search</label>
             <div id="pubsearch">
                 <div class="checkbox">
-                    <label><input type="checkbox" name="pubsearch_enable" value="pubsearch_enable" checked>Pubsearch enabled</label>
+                    <label><input type="checkbox" name="pubsearch_enable" value="pubsearch_enable"<?php if ($Userconfig->get_pubsearch_enabled()) {echo " checked";}?>>Public Search Enabled</label>
                 </div>
                 <div class="checkbox">
-                    <label><input type="checkbox" name="pubsearch_qslr_visible" value="pubsearch_qslr_visible" checked>QSL Received public visible</label>
+                    <label><input type="checkbox" name="pubsearch_qslr_visible" value="pubsearch_qslr_visible"<?php if ($Userconfig->get_pubqslr_enabled()) {echo " checked";}?>>QSL Received public visible</label>
                 </div>
                 <div class="checkbox">
-                    <label><input type="checkbox" name="pubsearch_qsls_visible" value="pubsearch_qsls_visible" checked>QSL Sent visible</label>
+                    <label><input type="checkbox" name="pubsearch_qsls_visible" value="pubsearch_qsls_visible"<?php if ($Userconfig->get_pubqsls_enabled()) {echo " checked";}?>>QSL Sent visible</label>
                 </div>
             </div>
         </div>
