f65b0bab9a804359c7fe7e978ee8adac8cc5b91d
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2024-07-03 13:39:27 +0200

    header.php: fix indentation

--- inc/header.php
+++ inc/header.php
@@ -7,38 +7,28 @@ $Cqrlog_common = new Cqrlog_common($DbSpawner->newdb());
 $Checkdxcc = new Checkdxcc("https://api.dl8bh.de/lookup/json/");
 $hamqthtimeout = FALSE;
 include("inc/userconfig.php");
-if (isset($_GET['log_id'])){
-$log_id = filter_input(INPUT_GET, 'log_id', FILTER_VALIDATE_INT);
-if (!is_int($log_id))
-		{
-				$log_id=$defaultlog;
-		}
-if (!$Cqrlog_common->logid_to_call($log_id))
-		{
-				$log_id=$defaultlog;
-		}
-
-}
-else
-{
-	$log_id=$defaultlog;
+if (isset($_GET['log_id'])) {
+	$log_id = filter_input(INPUT_GET, 'log_id', FILTER_VALIDATE_INT);
+	if (!is_int($log_id)) {
+		$log_id = $defaultlog;
+	}
+	if (!$Cqrlog_common->logid_to_call($log_id)) {
+		$log_id = $defaultlog;
+	}
+} else {
+	$log_id = $defaultlog;
 }
 $Userconfig = new Userconfig($DbSpawner->newdb(), $log_id, FALSE);
-if (isset($_GET['qso_count'])){
-$qso_count = filter_input(INPUT_GET, 'qso_count', FILTER_VALIDATE_INT);
-if (!is_int($qso_count))
-{
-		 $qso_count=$defaultcount;
-}
-}
-else
-{
-	$qso_count=$defaultcount;
+if (isset($_GET['qso_count'])) {
+	$qso_count = filter_input(INPUT_GET, 'qso_count', FILTER_VALIDATE_INT);
+	if (!is_int($qso_count)) {
+		$qso_count = $defaultcount;
+	}
+} else {
+	$qso_count = $defaultcount;
 }
 
 
-if (isset($_GET['qso_id'])){
-$qso_id = filter_input(INPUT_GET, 'qso_id', FILTER_VALIDATE_INT);
+if (isset($_GET['qso_id'])) {
+	$qso_id = filter_input(INPUT_GET, 'qso_id', FILTER_VALIDATE_INT);
 }
-
-?>
