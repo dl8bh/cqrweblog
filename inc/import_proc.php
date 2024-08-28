0e992e3da1b077f6be56866c8b995e6e1893b713
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2024-08-28 22:21:58 +0200

    import: use check_exact_dupe on import

--- inc/import_proc.php
+++ inc/import_proc.php
@@ -5,7 +5,7 @@ if (!empty($_FILES["import_adif_file"]["tmp_name"])) {
 	$filestring = file_get_contents($uploaded_file);
 	$adif_table = $adif_parser->read_adif_file($filestring);
 	foreach ($adif_table as $qso) {
-		if ($Logbook->check_dupe($qso)) {
+		if ($Logbook->check_exact_dupe($qso)) {
 		} else {
 			$Logbook->insert_qso($qso);
 		}
