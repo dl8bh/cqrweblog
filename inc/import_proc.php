eb969a7d0fbb9094e3a30960d4ba2ee3a4ad4007
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2024-08-28 16:04:22 +0200

    implement dupe-check in import

--- inc/import_proc.php
+++ inc/import_proc.php
@@ -4,5 +4,8 @@ $uploaded_file = $_FILES["import_adif_file"]["tmp_name"];
 $filestring = file_get_contents($uploaded_file);
 $adif_table = $adif_parser->read_adif_file($filestring);
 foreach ($adif_table as $qso) {
-	$Logbook->insert_qso($qso);
+	if ($Logbook->check_dupe($qso)) {
+	} else {
+		$Logbook->insert_qso($qso);
+	}
 }
