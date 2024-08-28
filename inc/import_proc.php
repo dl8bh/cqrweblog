b277e55071785ea97f65e4e3e9b0a56a6ab8a409
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2024-08-28 16:15:17 +0200

    import: adding a simple check, if something has uploaded

--- inc/import_proc.php
+++ inc/import_proc.php
@@ -1,11 +1,13 @@
 <?php
 $adif_parser = new Adif();
-$uploaded_file = $_FILES["import_adif_file"]["tmp_name"];
-$filestring = file_get_contents($uploaded_file);
-$adif_table = $adif_parser->read_adif_file($filestring);
-foreach ($adif_table as $qso) {
-	if ($Logbook->check_dupe($qso)) {
-	} else {
-		$Logbook->insert_qso($qso);
+if (!empty($_FILES["import_adif_file"]["tmp_name"])) {
+	$uploaded_file = $_FILES["import_adif_file"]["tmp_name"];
+	$filestring = file_get_contents($uploaded_file);
+	$adif_table = $adif_parser->read_adif_file($filestring);
+	foreach ($adif_table as $qso) {
+		if ($Logbook->check_dupe($qso)) {
+		} else {
+			$Logbook->insert_qso($qso);
+		}
 	}
 }
