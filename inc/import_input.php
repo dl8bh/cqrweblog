8bf597ca4a58f73c1451dc219eb9f8ef7a68e70e
Bernhard Kneip <bernhard.kneip@postadigitale.de>
2024-08-29 17:21:38 +0200

    import: add implementation to "ignore dupes" button

--- inc/import_input.php
+++ inc/import_input.php
@@ -1,32 +1,30 @@
 <?php
 echo '<form class="form" role="form" action="import.php?log_id=' . $log_id . '&qso_count=' . $qso_count . '" method="post", enctype="multipart/form-data">'
 ?>
+
 <div class="row">
+    <div class="col-sm-4"></div>
+    <div class="form-group col-sm-2 col-xs-4">
+        <label for="import_adif_file" class="control-label">File Upload</label>
+        <input class="form-control" type="file" id="import_adif_file" name="import_adif_file">
+    </div>
 
-    <div class="col-md-5 col-sm-5 col-xs-4"></div>
-    <div class="col-md-2 col-sm-2 col-xs-4">
+    <div class="form-group col-sm-1 col-xs-4">
 
-        <div class="form-group col-md-15 col-sm-13 col-xs-16">
-            <label for="import_adif_file" class="control-label">File Upload</label>
-            <input class="form-control" type="file" id="import_adif_file" name="import_adif_file">
+        <label for="import_settings" class="control-label">Import Settings</label>
+        <div id="import_settings">
+
+            <div class="checkbox">
+                <label><input type="checkbox" name="import_settings[]" value="ignore_dupes">Ignore Dupes</label>
+            </div>
+
 
         </div>
-
-
-
     </div>
-    <div class="col-md-1 col-sm-1"></div>
 </div>
-
 <div class="row">
-
-    <div class="col-md-5 col-sm-5 col-xs-4"></div>
-    <div class="col-md-2 col-sm-2 col-xs-4">
-        <div class"checkbox">
-            <label><input type="checkbox" name="ignore_dupes" value="ignore_dupes">Ignore Dupes</label>
-        </div>
-        <input class="btn btn-primary col-sm-12 col-xs-12 " type="submit" value="Import">
-    </div>
+    <div class="col-sm-5"></div>
+    <input class="btn btn-primary col-sm-1 col-xs-12" type="submit" value="Import">
 </div>
 
 </form>
--- inc/import_proc.php
+++ inc/import_proc.php
@@ -1,13 +1,24 @@
 <?php
 $adif_parser = new Adif();
 if (!empty($_FILES["import_adif_file"]["tmp_name"])) {
-	$uploaded_file = $_FILES["import_adif_file"]["tmp_name"];
-	$filestring = file_get_contents($uploaded_file);
-	$adif_table = $adif_parser->read_adif_file($filestring);
-	foreach ($adif_table as $qso) {
-		if ($Logbook->check_exact_dupe($qso)) {
-		} else {
-			$Logbook->insert_qso($qso);
-		}
-	}
+    $ignore_dupes = false;
+    if (isset($_POST["import_settings"])) {
+        $import_settings = $_POST["import_settings"];
+        if (in_array("ignore_dupes", $import_settings)) {
+            $ignore_dupes = true;
+        }
+    }
+    $uploaded_file = $_FILES["import_adif_file"]["tmp_name"];
+    $filestring = file_get_contents($uploaded_file);
+    $adif_table = $adif_parser->read_adif_file($filestring);
+    foreach ($adif_table as $qso) {
+        if ($ignore_dupes) {
+            $Logbook->insert_qso($qso);
+        } else {
+            if ($Logbook->check_exact_dupe($qso)) {
+            } else {
+                $Logbook->insert_qso($qso);
+            }
+        }
+    }
 }
